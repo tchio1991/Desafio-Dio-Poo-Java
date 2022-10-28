d4cd7c4ab799e110c1cfd35b23bae2efa5f42929
Thiago Cardoso <109465375+tchio1991@users.noreply.github.com>
2022-10-28 12:34:10 -0300

    Added Project

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,29 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso extends Conteudo{
+
+    double cargaHoraria;
+
+
+    @Override
+    protected double calculoXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public double getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(double cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "\nCurso{" +
+                "\nNome Curso = " + nomeConteudo +
+                ",\nDescrição Curso = '" + descricaoConteudo + '\'' +
+                ",\nCarga Horaria Curso = '" + cargaHoraria + '\'' +
+                '}';
+    }
+}
